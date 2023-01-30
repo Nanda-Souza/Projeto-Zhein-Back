b4f4f3b5de90220885472b996217c7b17a23be4a
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-01-30 12:44:40 -0300

    refactory; incompleto

--- src/controllers/carrinhoController.js
+++ src/controllers/carrinhoController.js
@@ -1,12 +1,13 @@
-import { cartCollection } from "../config/Database.js";
+import { cartCollection, sessoesCollection } from "../config/Database.js";
 import { ObjectId } from "mongodb";
 
 
 export async function postCarrinho(req, res){
-    const {url, nome, preco} = req.body;
+    const algumaCoisa = req.body;
+    console.log(algumaCoisa);
 
     try{
-        await cartCollection.insertOne({url, nome, preco});
+        await cartCollection.insertOne({algumaCoisa});
         res.sendStatus(201);
     }catch(err){
         res.status(500).send(err);
@@ -14,8 +15,22 @@ export async function postCarrinho(req, res){
 };
 
 export async function getCarrinho(req, res) {
+  const {authorization} = req.headers;
+
+  const token = authorization?.replace("Bearer ", "");
+
+  if(!token)return res.status(422).send("Informe o token!");
+
   try {
-    const listaCarrinho = await cartCollection.find().toArray();
+    const userExist = await sessoesCollection.findOne({token});
+
+    if(!userExist)return res.status(401).send("Você não tem autorização para acessar o carrinho");
+
+
+    const listaCarrinho = await cartCollection.find({_id: userExist.idUsuario}).toArray();
+
+    if(!listaCarrinho)return res.sendStatus(401);
+
     res.send(listaCarrinho);
   } catch (err) {
     res.status(500).send(err);
